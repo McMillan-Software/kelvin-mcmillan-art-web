bbe77c2dcfcd0b781d783ecccafb58f2075b6070
Gussssy <angus.mcmillan.94@gmail.com>
2025-03-13 07:48:13 +1300

    Add giclee flow WIP

--- src/components/CreatePainting/CreatePainting.tsx
+++ src/components/CreatePainting/CreatePainting.tsx
@@ -1,4 +1,4 @@
-import React, { useState  } from "react";
+import React, { useEffect, useState  } from "react";
 import axios from "axios";
 import './CreatePainting.css';
 import { useAuth }  from "../../AuthContext";
@@ -8,7 +8,7 @@ const paintingTypes = ["Watercolour", "Acrylic"];
 const pageOptions = ["Marine", "Rural", "Landscape"];
 
 const CreatePainting: React.FC = () => {
-    const { isAuthenticated, login} = useAuth();
+    const { isAuthenticated, login} = useAuth(); // why { } here 
     const[error, setError] = useState("");
     const [title, setTitle] = useState("");
     const [type, setType] = useState("");
@@ -24,11 +24,13 @@ const CreatePainting: React.FC = () => {
     const [imagePreview, setImagePreview] = useState<string>("");
     const [pages, setPages] = useState<string[]>([]);
 
+    const [availableAspectRatios, setAvailableAspectRatios] = useState([]);
+
     const handlePaintingCreation = async (e: React.FormEvent) => {
         e.preventDefault();
         try {
             const response = await axios.post(
-                'http://localhost:8000/admin/painting', 
+                'http://localhost:8000/admin/painting', // will only work when running locally...
                 {
                   title,
                   type,
@@ -51,7 +53,7 @@ const CreatePainting: React.FC = () => {
                 const formData = new FormData();
                 formData.append("file", image);
                 await axios.post(
-                    `${import.meta.env.VITE_API_URL}painting/${response.data.id}/image`,
+                    `${import.meta.env.VITE_API_URL}admin/painting/${response.data.id}/image`,
                     formData,
                     {
                         headers: {
@@ -93,6 +95,37 @@ const CreatePainting: React.FC = () => {
     if (!isAuthenticated) {
         return <div>User must log in</div>;
     }
+
+
+
+    useEffect (() => {
+
+        console.log("Aspect Ratio drop down updated");
+
+        const fetchAspectRatios = async () => {
+            try {
+                console.log("Fetching aspect ratios...");
+                const token = localStorage.getItem("token"); // surely there is a better way to holdthe token then getting it each time...?
+                const response = await axios.get(`${import.meta.env.VITE_API_URL}admin/aspectratios`, {
+                    headers: {
+                        Authorization: `Bearer ${token}` // ✅ Include the token
+                    }
+                });
+                console.log("Response:", response.data);
+                setAvailableAspectRatios( response.data)
+
+            } catch (error) {
+                console.error("Error fetcghing aspect ratios");
+            }
+        };
+
+        fetchAspectRatios();
+
+    }, [] );
+
+
+
+
 
     return (
         <div className="painting-creation-div">
@@ -208,6 +241,25 @@ const CreatePainting: React.FC = () => {
                             }}/>
                     <input type="file" accept="image/jpeg, image/png" onChange={handleImageUpload} />
                 </div>
+               
+            </div>
+            <div className="giclee-create">
+                <h2>Giclee Create</h2>
+                <div className="set-aspect-ratio"></div>
+                <p>Set Aspect Ratio</p>
+                <label htmlFor="aspect-ratio"> Aspect Ratio:</label>
+                <select 
+                    id='aspect-ratio'
+                    value={aspectRatio}
+                    onChange={(e) => setAspectRatio(e.target.value)}
+                >
+                    <option value="">Select an Aspect Ratio</option>
+                    {availableAspectRatios.map((ratio, index) => (
+                        <option key={index} value={ratio}>
+                            {ratio}
+                        </option>
+                    ))}
+                </select>
             </div>
         </div>
     );
