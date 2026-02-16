a9ccd93a3337f3e4d567e89247ad0941983c1ba2
Sam <sammcmillan97@gmail.com>
2026-02-17 06:33:08 +1300

    Fix issues caused by auto merge

--- src/components/Original/Original.tsx
+++ src/components/Original/Original.tsx
@@ -33,30 +33,20 @@ const Original: React.FC = () => {
           <div className="original-image-container">
             <img className="orginal-image" src={`${import.meta.env.VITE_IMAGE_BASE_PATH}${original.imagePath}`} alt={original.title} />
           </div>
-          <h2>{original.title}</h2>
-          <p>{original.info}</p>
+            <h2>{original.title}</h2>
+            <p className="original-info">{original.info}</p>
 
-          <p>{original.type} - ${original.price} - {original.width}x{original.height} </p>
-          {original.galleryLink ? (
-            <p>Available at <Link to={`https://${original.galleryLink}`} target="_blank" rel="noopener noreferrer">{original.galleryName}</Link></p>
-          ) : (
-            <br/>
-          )}
-        </div>
-        <h2>{original.title}</h2>
-        <p className="original-info">{original.info}</p>
-
-        <p>
-          {original.type}
-          {original.price ? ` - $${original.price}` : ''}
-          {` - ${original.width}mm x ${original.height}mm`}
-        </p>
-        {original.galleryLink ? (
-          <p>Available at <Link to={`https://${original.galleryLink}`} target="_blank" rel="noopener noreferrer">{original.galleryName}</Link></p>
-        ) : (
-          <br />
-        )}
-      </div>
+            <p>
+              {original.type}
+              {original.price ? ` - $${original.price}` : ''}
+              {` - ${original.width}mm x ${original.height}mm`}
+            </p>
+            {original.galleryLink ? (
+              <p>Available at <Link to={`https://${original.galleryLink}`} target="_blank" rel="noopener noreferrer">{original.galleryName}</Link></p>
+            ) : (
+              <br />
+            )}
+          </div>
     </div>
   );
 }
